28d59c172fc32ae34533e6a267ec0bf09835f85c
antoniamikey <antonia-schikora@outlook.de>
2026-04-15 16:57:05 +0200

    done until Nebenbedingungen

--- Abschlussprojekt/Abschlussprojekt.py
+++ Abschlussprojekt/Abschlussprojekt.py
@@ -20,89 +20,107 @@ lastgang = xp.problem()
 
 ### Skalarparameter ###########
 
-# Anlasskosten
-ako1 = use.read_scalar(sheet, "N2") # Anlasskosten für Generator Typ 1
-ako2 = use.read_scalar(sheet, "O2") # Anlasskosten für Generator Typ 2 
-
-# Kosten auf Minimallast pro Stunde
-minlastko1 = use.read_scalar(sheet, "N3") # Anlasskosten für Generator Typ 1
-minlastko2 = use.read_scalar(sheet, "O3") # Anlasskosten für Generator Typ 2 
-
-# Kostenoberhalb der Minimallast pro MW pro Stunde
-kouebermin1 = use.read_scalar(sheet, "N4") # Anlasskosten für Generator Typ 1
-kouebermin2 = use.read_scalar(sheet, "O4") # Anlasskosten für Generator Typ 2 
-
-# Minimallast in MW
-minlast1 = use.read_scalar(sheet, "N5") # Anlasskosten für Generator Typ 1
-minlast2 = use.read_scalar(sheet, "O5") # Anlasskosten für Generator Typ 2 
-
-# Maximallast in MW
-maxlast1 = use.read_scalar(sheet, "N6") # Anlasskosten für Generator Typ 1
-maxlast2 = use.read_scalar(sheet, "O6") # Anlasskosten für Generator Typ 2 
-
-# Anzahl verfügbarer Generatoren
-vergen1 = use.read_scalar(sheet, "N7") # Anlasskosten für Generator Typ 1
-vergen2 = use.read_scalar(sheet, "O7") # Anlasskosten für Generator Typ 2 
+# Lastgangpuffer, wie viel Spielraum ist nach oben und nach unten gegeben
+puffer_oben = use.read_scalar(sheet, "J2") 
+puffer_unten = use.read_scalar(sheet, "K2")
 
 ### Mengen #####################
-
-# Tageszeit
-ZEIT = use.read_index(sheet, "A3", "vertical", 24)
 
 # Wochentag
 TAG = use.read_index(sheet, "B2", "horizontal", 7)
 
+# Tageszeit
+STUNDE = use.read_index(sheet, "A3", "vertical", 24)
+
+# Generatortypen -> hier Zahl ändern, wenn mehr Generatortypen verwendet werden sollen
+GEN = use.read_index(sheet, "N1", "horizontal", 2)
+
 ### Vektorparameter ############
 
-# Transportkosten
-bedarf = use.read_table(sheet, "B3", TAG, ZEIT)
+# Anlasskosten (EUR pro Anlassung)
+anlassko = use.read_table(sheet, "N2", GEN)
+
+# Fixkosten auf Minimallast (EUR pro Stunde je Generator)
+minlastko = use.read_table(sheet, "N3", GEN)
+
+# Variable Kosten oberhalb Minimallast (EUR pro MW und Stunde)
+kouebermin = use.read_table(sheet, "N4", GEN)
+
+# Minimallast (MW)
+minlast = use.read_table(sheet, "N5", GEN)
+
+# Maximallast (MW)
+maxlast = use.read_table(sheet, "N6", GEN)
+
+# Anzahl verfügbarer Generatoren
+verfgen = use.read_table(sheet, "N7", GEN)
+
+# Lastgang-Bedarf (MW)
+bedarf = use.read_table(sheet, "B3", STUNDE, TAG)
 
 
 ################################
 ### VARIABLEN                ###
 ################################
 
-# Produktionsmenge pro Periode -> AUS VORHERIGEM PROJEKT; MUSS NOCH GEÄNDERT WERDEN
-x = lastgang.addVariables(PERIODE, name="x", 
+# Gesamtproduktion aller Generatoren von Typ g in Stunde h an Tag t (in MW)
+ProdV = lastgang.addVariables(GEN, STUNDE, TAG, name="ProdV", 
                      lb=0, vartype = xp.continuous)
 
+# Anzahl laufender Generatoren von Typ g in Stunde h an Tag t (ganzzahlig)
+LaufGV = lastgang.addVariables(GEN, STUNDE, TAG, name="LaufGV", 
+                     lb=0, vartype = xp.integer)
 
+# Anzahl neu gestarteter Generatoren von Typ g in Stunde h an Tag t (ganzzahlig)
+NeuGV = lastgang.addVariables(GEN, STUNDE, TAG, name="NeuGV", 
+                     lb=0, vartype = xp.integer)
+
+# Netzwerkstabilität (NetzV[STUNDE, TAG] = 1 wenn mehr als 3 Generatoren vom Typ 1 verwendet werden)
+NetzV = lastgang.addVariables(STUNDE, TAG, name="NetzV", 
+                          lb=0, vartype=xp.binary)
 
 
 ################################
 ### ZIELFUNKTION             ###
 ################################
 
-# Minimierung der Produktionskosten über alle Perioden-> AUS VORHERIGEM PROJEKT; MUSS NOCH GEÄNDERT WERDEN
-produktionskosten = xp.Sum(x[p] * pk +
-                    l[p] * lk +
-                    erh[p] * ek +
-                    verm[p] * vk
-                    for p in PERIODE)
+# Minimierung der Gesamtkosten
+gesamtkosten = (
+    
+    # Anlasskosten
+    xp.Sum(anlassko[i] * NeuGV[i, h, t] for i in GEN for h in STUNDE for t in TAG)
+
+    # + Kosten auf Minimallast
+    + xp.Sum(minlastko[i] * LaufGV[i, h, t] for i in GEN for h in STUNDE for t in TAG)
+
+    # + Kosten über Minimallast 
+    + xp.Sum(kouebermin[i] * (ProdV[i, h, t] - minlast[i] * LaufGV[i, h, t]) for i in GEN for h in STUNDE for t in TAG)
+)
 
 # Hinzufügen der Zielfunktion zum Modell 
-lastgang.setObjective(kosten, sense=xp.minimize)
+lastgang.setObjective(gesamtkosten, sense=xp.minimize)
 
 
 ################################
 ### NEBENBEDINGUNGEN         ###
 ################################
 
-######-> AUS VORHERIGEM PROJEKT; MUSS NOCH GEÄNDERT WERDEN
+# Lastgang muss mindestens dem Bedarf entsprechen
+bedarfsdeckung = [xp.Sum(ProdV[i, h, t] for i in GEN) >= bedarf[h, t] 
+                   for h in STUNDE for t in TAG] 
 
-# Anpassung der Produktionsmenge an den Bedarf -> Änderungsmenge für die erste Periode wird nicht betrachtet, da diese immer 0 ist
-produktionsmenge = [x[PERIODE[i]] + l[PERIODE[i-1]] >= bedarf[PERIODE[i]] 
-                   for i in range(1,I)] 
-
-# Veränderungen in der Produktionsmenge, welche ebenfalls Kosten verursachen 
+# Lastgang muss die Puffer einhalten
 bilanz = [erh[PERIODE[i]] - verm[PERIODE[i]] == x[PERIODE[i]] - x[PERIODE[i-1]]
              for i in range(1, I)] 
 
-# Berechnung des Lagerbestands 
-lager_start = l0 + x[PERIODE[0]] - bedarf[PERIODE[0]]==l[PERIODE[0]]  # Lagerbestand wird für die erste Periode gegeben
+# es muss Netzwerkstabilität gewährleistet werden -> es ist nicht möglich, mehr als 3 Generatoren vom Typ 2 zu verwenden, wenn zu demselben Zeitpunkt mehr als 3 Generatoren vom Typ 1 verwendet werden.
+lager_start = l0 + x[PERIODE[0]] - bedarf[PERIODE[0]]==l[PERIODE[0]]  
 
+# Lastgang zum Ende der Woche muss dem Lastgang zu Beginn der nächsten Periode entsprechen 
 lager = [x[PERIODE[i]] + l[PERIODE[i-1]] - bedarf[PERIODE[i]] == l[PERIODE[i]]
          for i in range(1, I)] # Lagerbestand für die weiteren Perioden
+
+# Kostensachen
 
 # Nebenbedingungen zur Instanz hinzufügen
 lastgang.addConstraint()
